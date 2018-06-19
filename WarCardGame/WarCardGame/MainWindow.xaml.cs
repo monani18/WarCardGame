9ce427a6727328583e1451e1c33c9aa4f6aeb432
Mona Ni <monani18@outlook.com>
2018-06-18 23:12:52 -0400

    GiveCards distributes cards to the winner of the battle + testing code

--- WarCardGame/WarCardGame/MainWindow.xaml.cs
+++ WarCardGame/WarCardGame/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace WarCardGame
         List<Card> allCards { get; set; }
         List<Card> shuffled { get; set; }
         List<Card>[] playerDecks { get; set; }
+        List<Card> battleCards { get; set; }
+        List<Card>[] playerDecksDown { get; set; }
         
         //UI is built for 2-player. Code is scalable.
         int numPlayers { get; set; }
@@ -48,6 +50,7 @@ namespace WarCardGame
             shuffled = Shuffle(allCards);
 
             playerDecks = new List<Card>[numPlayers];
+            playerDecksDown = new List<Card>[numPlayers];
             playerDecks = DealCards(shuffled, numPlayers);
 
             for (int i = 0; i < playerDecks.Length; i++)
@@ -66,25 +69,28 @@ namespace WarCardGame
         {
             Console.WriteLine("Battle button pressed!");
 
-            List<Card> battleCards = new List<Card>();
+            battleCards = new List<Card>();
             int victor = new int();
 
-            ////Collect cards for battle and keep them in order
-            //for (int i=0; i<numPlayers; i++)
-            //{
-            //    Console.WriteLine(playerDecks[i][0].number + " " + playerDecks[i][0].suit);
-            //    battleCards.Add(playerDecks[i][0]);
-            //}
+            //Collect cards for battle and keep them in order
+            Console.WriteLine("Battle cards:");
+            for (int i = 0; i < numPlayers; i++)
+            {
+                Console.WriteLine(playerDecks[i][0].number + " " + playerDecks[i][0].suit);
+                battleCards.Add(playerDecks[i][0]);
+                playerDecks[i].RemoveAt(0);
+            }
 
-            Card card0 = new Card();
-            Card card1 = new Card();
-            card0.number = 4;
-            card0.suit = 'S';
-            card1.number = 3;
-            card1.suit = 'D';
-            battleCards.Add(card0);
-            battleCards.Add(card1);
+            //Card card0 = new Card();
+            //Card card1 = new Card();
+            //card0.number = 4;
+            //card0.suit = 'S';
+            //card1.number = 3;
+            //card1.suit = 'D';
+            //battleCards.Add(card0);
+            //battleCards.Add(card1);
 
+            Console.WriteLine("Check on battle cards:");
             for (int i=0; i<battleCards.Count; i++)
             {
                 Console.WriteLine(battleCards[i].number + " " + battleCards[i].suit);
@@ -98,17 +104,16 @@ namespace WarCardGame
             {
                 victor = firstLastMax[0];
                 Console.WriteLine("Victor is Player " + victor);
+                //GiveCards(victor);
             }
             else
             {
                 Console.WriteLine("WAR!");
+
             }
-            
 
             //Give cards to victor
-
-            //int victor = findVictor(
-
+            GiveCards(victor);
         }
 
         private List<Card> InitCards()
@@ -145,7 +150,8 @@ namespace WarCardGame
             int numCards = shuffledCards.Count;
             for (int i = 0; i < numCards; i++)
             {
-                int r = rand.Next(0, numCards);
+                //int r = rand.Next(0, numCards);
+                int r = rand.Next(0, i + 1);
                 Card temp = shuffledCards[r];
                 shuffledCards[r] = shuffledCards[i]; //replaces rth card with 0th card
                 shuffledCards[i] = temp; //replaces 0th card with former rth card
@@ -156,10 +162,11 @@ namespace WarCardGame
         private List<Card>[] DealCards(List<Card> cards, int numDecks)
         {
             List<Card> undealtCards = new List<Card>(cards);
-            List<Card>[] decks = new List<Card>[numDecks];
+            List<Card>[] playerDecks = new List<Card>[numDecks];
             for (int i=0; i<numDecks; i++)
             {
-                decks[i] = new List<Card>();
+                playerDecks[i] = new List<Card>();
+                playerDecksDown[i] = new List<Card>();
             }
 
             int d = 0;
@@ -168,14 +175,14 @@ namespace WarCardGame
             for (int u=0; u<totalCards; u++)
             {
                 //Deal card 0 (top of deck) to player (last card in deck)
-                decks[d].Add(undealtCards[0]);
+                playerDecks[d].Add(undealtCards[0]);
                 undealtCards.RemoveAt(0);
                 
                 //Change deck that next card is dealt to
                 d = (d < numDecks - 1) ? d+1 : 0;
             }
 
-            return decks;
+            return playerDecks;
         }
 
         //Finds the first and last occurrences of the max of a list of cards
@@ -193,9 +200,34 @@ namespace WarCardGame
             firstLastMax[1] = Array.LastIndexOf(numbers, max);
             Console.WriteLine("First index of max: " + firstLastMax[0]);
             Console.WriteLine("Last index of max: " + firstLastMax[1]);
-
-            //UNTESTED as of 6/17/18 at 2:40AM
+            
             return firstLastMax;
+        }
+        
+        private void GiveCards(int victor)
+        {
+            Console.WriteLine("Number of battle cards: " + battleCards.Count);
+
+            Console.WriteLine("Cards won in battle: ");
+            for (int i = 0; i < battleCards.Count; i++)
+            {
+                Console.WriteLine(battleCards[i].number + " " + battleCards[i].suit);
+            }
+
+            int totalBattleCards = battleCards.Count;
+            
+            for (int i=0; i<totalBattleCards; i++)
+            {
+                playerDecksDown[victor].Add(battleCards[0]);
+                battleCards.RemoveAt(0);
+                Console.WriteLine("i=" + i);
+            }
+
+            Console.WriteLine("Cards won by player " + victor + ":");
+            for (int i=0; i<playerDecksDown[victor].Count; i++)
+            {
+                Console.WriteLine(playerDecksDown[victor][i].number + " " + playerDecksDown[victor][i].suit);
+            }
         }
     }
 }
